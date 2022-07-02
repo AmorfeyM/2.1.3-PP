31071c11c0ab3096b45fc7246ff36a048e106c89
Sergey Popov <popovsv1987@gmail.com>
2022-07-02 16:12:56 +0300

    initial commit

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,11 @@
 package app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
+
 public class AppConfig {
 }
